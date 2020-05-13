c73551f00aec3aa5f6622ab9e275dd46ae70631b
JohnOrt31 <jj.coronaortega@ugto.mx>
2020-05-13 03:35:56 -0500

    Se crearon métodos CRUD para books, users, zips

--- mvc_library_db/model/model.py
+++ mvc_library_db/model/model.py
@@ -22,6 +22,240 @@ class Model:
     
     def connect_to_db(self):
         self.cnx = connector.connect(**self.config_db)
+        self.cursor = self.cnx.cursor() #Para consultas u operaciones en MySQL
 
     def close_db(self):
         self.cnx.close()
+
+    """
+    ****************
+    * ZIP methods *
+    ****************
+    """
+
+    def create_zip(self, zip, city, state):
+        try: 
+            sql = 'INSERT INTO zips (`zip`, `z_city`, `z_state`) VALUES (%s, %s, %s)'
+            vals = (zip, city, state)
+            self.cursor.execute(sql, vals)
+            self.cnx.commit()
+            return True
+        except connector.Error as err:
+            self.cnx.rollback()
+            return err
+
+    def read_a_zip(self, zip):
+        try:
+            sql = 'SELECT * FROM zips WHERE zip = %s'
+            vals = (zip,)
+            self.cursor.execute(sql, vals)
+            record = self.cursor.fetchone()
+            return record
+        except connector.Error as err:
+            return err
+
+    def read_all_zips(self):
+        try:
+            sql = 'SELECT * FROM zips'
+            self.cursor.execute(sql)
+            records = self.cursor.fetchall()
+            return records
+        except connector.Error as err:
+            return err
+    
+    def read_zips_city(self, city):
+        try:
+            sql = 'SELECT * FROM zips WHERE z_city = %s'
+            vals = (city,)
+            self.cursor.execute(sql, vals)
+            records = self.cursor.fetchall()
+            return records
+        except connector.Error as err:
+            return err
+    
+    def update_zip(self, fields, vals):
+        try:
+            sql = 'UPDATE zips SET'+','.join(fields)+'WHERE zip = %s'
+            self.cursor.execute(sql,vals)
+            self.cnx.commit()
+            return True
+        except connector.Error as err:
+            self.cnx.rollback()
+            return err
+
+    def delete_zip(self, zip):
+        try:
+            sql = 'DELETE FROM zips WHERE zip = %s'
+            vals = (zip,)
+            self.cursor.execute(sql, vals)
+            self.cnx.commit()
+            count = self.cursor.rowcount
+            return count
+        except connector.Error as err:
+            self.cnx.rollback()
+            return err
+    
+
+    """
+    ****************
+    * Books methods *
+    ****************
+    """
+
+    def create_book(self, isbn, title, author, editorial, edition, publication, category, description, language, pages, shelving, quantity):
+        try: 
+            sql = 'INSERT INTO books (`isbn`, `b_title`, `b_author`, `b_editorial`, `b_edition`, `b_publidate`, `b_category`, `b_description`, `b_language`, `b_pages`, `b_shelving`, `b_quantity`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+            vals = (isbn, title, author, editorial, edition, publication, category, description, language, pages, shelving, quantity)
+            self.cursor.execute(sql, vals)
+            self.cnx.commit()
+            return True
+        except connector.Error as err:
+            self.cnx.rollback()
+            return err
+    
+    def read_a_book(self, isbn):
+        try:
+            sql = 'SELECT * FROM books WHERE isbn = %s'
+            vals = (isbn,)
+            self.cursor.execute(sql, vals)
+            record = self.cursor.fetchone()
+            return record
+        except connector.Error as err:
+            return err
+
+    def read_all_books(self):
+        try:
+            sql = 'SELECT * FROM books'
+            self.cursor.execute(sql)
+            records = self.cursor.fetchall()
+            return records
+        except connector.Error as err:
+            return err
+
+    def read_books_author(self, author):
+        try:
+            sql = 'SELECT  * FROM books WHERE b_author = %s'
+            vals = (author,)
+            self.cursor.execute(sql, vals)
+            records = self.cursor.fetchall()
+            return records
+        except connector.Error as err:
+            return err
+
+    def read_books_pages_range(self, pages_min, pages_max):
+        try:
+            sql = 'SELECT * FROM books WHERE b_quantity >= %s and b_quantity <= %s'
+            vals = (pages_min, pages_max)
+            self.cursor.execute(sql, vals)
+            records = self.cursor.fetchall()
+            return records
+        except connector.Error as err:
+            return err
+    
+    def update_book(self, fields, vals):
+        try:
+            sql = 'UPDATE books SET'+','.join(fields)+'WHERE isbn = %s'
+            self.cursor.execute(sql,vals)
+            self.cnx.commit()
+            return True
+        except connector.Error as err:
+            self.cnx.rollback()
+            return err
+
+    def delete_book(self, isbn):
+        try:
+            sql = 'DELETE FROM books WHERE isbn = %s'
+            vals = (isbn,)
+            self.cursor.execute(sql, vals)
+            self.cnx.commit()
+            count = self.cursor.rowcount
+            return count
+        except connector.Error as err:
+            self.cnx.rollback()
+            return err
+
+    """
+    ****************
+    * User methods *
+    ****************
+    """
+    def create_user(self, name, flname, slname, email, phone, street, noext, noint, col, zip):
+        try: 
+            sql = 'INSERT INTO users (`u_fname`, `u_lname1`, `u_lname2`, `u_email`, `u_phone`, `u_street`, `u_noext`, `u_noint`, `u_col`, `u_zip`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+            vals = (name, flname, slname, email, phone, street, noext, noint, col, zip)
+            self.cursor.execute(sql, vals)
+            self.cnx.commit()
+            return True
+        except connector.Error as err:
+            self.cnx.rollback()
+            return err
+        
+    def read_a_user(self, id_user):
+        try:
+            sql = 'SELECT users.*,zips.z_city,zips.z_state FROM users JOIN zips ON users.u_zip = zips.zip and users.id_user = %s'
+            vals = (id_user,)
+            self.cursor.execute(sql, vals)
+            record = self.cursor.fetchone()
+            return record
+        except connector.Error as err:
+            return err
+
+    def read_all_users(self):
+        try:
+            sql = 'SELECT users.*,zips.z_city,zips.z_state FROM users JOIN zips ON users.u_zip = zips.zip'
+            self.cursor.execute(sql)
+            records = self.cursor.fetchall()
+            return records
+        except connector.Error as err:
+            return err
+
+    def read_users_zip(self, zip):
+        try:
+            sql = 'SELECT users.*,zips.z_city,zips.z_state FROM users JOIN zips ON users.u_zip = zips.zip and users.u_zip = %s'
+            vals = (zip,)
+            self.cursor.execute(sql, vals)
+            records = self.cursor.fetchall()
+            return records
+        except connector.Error as err:
+            return err
+
+    def update_user(self, fields, vals):
+        try:
+            sql = 'UPDATE users SET'+','.join(fields)+'WHERE id_user = %s'
+            self.cursor.execute(sql,vals)
+            self.cnx.commit()
+            return True
+        except connector.Error as err:
+            self.cnx.rollback()
+            return err
+
+    def delete_user(self, id_user):
+        try:
+            sql = 'DELETE FROM users WHERE id_user = %s'
+            vals = (id_user,)
+            self.cursor.execute(sql, vals)
+            self.cnx.commit()
+            count = self.cursor.rowcount
+            return count
+        except connector.Error as err:
+            self.cnx.rollback()
+            return err
+
+    """
+    ****************
+    * Loan methods *
+    ****************
+    """
+
+
+
+
+
+
+
+
+
+
+
+
+
